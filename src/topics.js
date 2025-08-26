26f8f1d84da3d2634130564801c351afa2e46c15
Katy Cassidy <ihatetoast@gmail.com>
2025-08-26 18:59:25 -0500

    Initial commit

---
+++ src/topics.js
@@ -0,0 +1,250 @@
+export default [
+  {
+    id: 't1',
+    topic: 'capitals',
+    questions: [
+      {
+        id: 't1q1',
+        question: 'What is the capital of Baden-Württemberg?',
+        answers: [
+          'Stuttgart',
+          'Baden-Württemberg is a city-state and it its own capital.',
+          'Ulm',
+          'Heidelberg',
+          'Badenschmaden',
+        ],
+        image:
+          'https://www.stuttgart.de/tourismus/sehenswuerdigkeiten/zoo-wilhelma.php.media/185080/Wilhelma-c-Thomas-Wagner-LHS.jpg.scaled/b10df0f4235ab0a3f738834383934fe3.jpg',
+        imageAlt: 'Wilhelma Zoo',
+      },
+      {
+        id: 't1q2',
+        question: 'What is the capital of Bavaria (Bayern)?',
+        answers: [
+          'Munich (München)',
+          'Augsberg',
+          'Ingolstadt',
+          'Regensburg',
+          'Oberammergau-Unterammergau',
+        ],
+        image:
+          'https://www.muenchen.de/sites/default/files/styles/3_2_w1216/public/2022-07/lhm-e-garten-panorama.jpg.webp',
+        imageAlt: 'English Gardens',
+      },
+      {
+        id: 't1q3',
+        question: 'What is the capital of Berlin?',
+        answers: [
+          'Berlin is a city-state and it its own capital',
+          'Berlinstadt',
+          'Ost-West-Berlinerhaven.',
+          'Bärland',
+        ],
+        image:
+          'https://www.berlin.de/binaries/asset/image_assets/9276987/source/1731061022/1000x500/',
+        imageAlt: 'fix later',
+      },
+      {
+        id: 't1q4',
+        question: 'What is the capital of Brandenburg?',
+        answers: [
+          'Potsdam',
+          'Brandenburg is a city-state and it its own capital',
+          'Oranienburg',
+          'Frankfurt',
+          'Parisdorf',
+        ],
+        image:
+          'https://www.potsdam.de/system/files/styles/potsdam_main_node_full_xl_1x/private/images/l_franzoesischekirche.jpg?h=b678530e&itok=HIlIou4M',
+        imageAlt: 'French church',
+      },
+      {
+        id: 't1q5',
+        question: 'What is the capital of Bremen?',
+        answers: [
+          'Bremen is a city-state officially known as the Free Hanseatic City of Bremen.',
+          'Havenbremer',
+          'Frankfurt',
+          'Bremen is not a German city. It was lost in a poker game against Liechtenstein.',
+        ],
+        image:
+          'https://medien.bremen.de/media/944/472/mfb2023-e---rathaus-dom--c--fotoetage.jpg',
+        imageAlt: 'Bremen city hall',
+      },
+      {
+        id: 't1q6',
+        question: 'What is the capital of Hamburg?',
+        answers: [
+          'Hamburg is a city-state officially known as the Free Hanseatic City of Hamburg.',
+          'Inner Alster',
+          'Elbe',
+          'Wandsbek',
+          'Rote Grützendorf',
+        ],
+        image:
+          'https://www.hamburg.de/resource/image/405908/landscape_ratio16x9/1240/697/7268480b9908af2e8603004f9363fea2/249BE1821EEECCD09D2DF57095BBF688/bg-1-kaiserspeicher.jpg',
+        imageAlt: "Hamburg's Maritimes Museum",
+      },
+      {
+        id: 't1q7',
+        question: 'What is the capital of Hesse (Hessen)?',
+        answers: [
+          'Wiesbaden',
+          'Darmstadt',
+          'Kassel',
+          'Offenbach am Main',
+          'Hesse is a city-state officially known as the Free Hanseatic City of Hessemesse.',
+        ],
+        image:
+          'https://www.wiesbaden.de/leben-in-wiesbaden/freizeit/natur-erleben/gruenflaechen/Kurpark.php.media/298762/20200421_121231.jpg.scaled/bcaade7839a8a67a0c7820636227bc34.jpg',
+        imageAlt:
+          'View of the Kurhaus from the Kurpark, looking over a pond surrouned by trees',
+      },
+      {
+        id: 't1q8',
+        question: 'What is the capital of Lower Saxony (Niedersachsen)?',
+        answers: [
+          'Hanover (Hannover)',
+          'Göttingen',
+          'Wolfsburg',
+          'Lüneburg',
+          'Goetheburg',
+        ],
+        image:
+          'https://www.hannover-tourismus.de/wp-content/uploads/elementor/thumbs/Herrenhausen_klein-r7hmwj9gh4ir4tmrf4jib4kn0t7ddce9jaxckdbrc0.jpg',
+        imageAlt:
+          'View of the Herrenhausen Gardens with a large fountain and gardens',
+      },
+      {
+        id: 't1q9',
+        question: 'What is the capital of Mecklenburg-Vorpommern?',
+        answers: [
+          'Schwerin',
+          'Stralsund',
+          'Greifswald',
+          'Wismar',
+          'Vierpommes',
+        ],
+        image:
+          'https://www.landtag-mv.de/fileadmin/_processed_/d/8/csm_Buga_Schlosspromenade_Lehmann_a2ad861286.jpg',
+        imageAlt: 'View of a castle on the banks of a river',
+      },
+      {
+        id: 't1q10',
+        question:
+          'What is the capital of Rhineland-Palatinate (Rheinland-Pfalz)?',
+        answers: [
+          'Mainz',
+          'Ludwigshafen am Rhein',
+          'Trier',
+          'Koblenz',
+          'Pfalzundseffer',
+        ],
+        image:
+          'https://www.mainz.de/medien/internet/img/thema/museen/gutenberg-museum/Roemischer_Kaiser_2_MartinaPipprich.jpg.scaled/034bce657231836d7553e1d6c70d1304.jpg',
+        imageAlt: 'Haus zum Römischer Kaiser',
+      },
+      {
+        id: 't1q11',
+        question:
+          'What is the capital of North Rhine-Westphalia (Nordrhein-Westfalen)?',
+        answers: [
+          'Düsseldorf',
+          'Dortmund',
+          'Essen',
+          'Bielefeld',
+          'Rheinischer-Sauerbraten',
+        ],
+        image:
+          'https://mein.toubiz.de/api/v1/media/b04b927b-3349-4bdf-922c-32f291bc5466/view?fm=768&w=m&h=e',
+        imageAlt: 'Glass dome in the botanical gardens',
+      },
+      {
+        id: 't1q12',
+        question: 'What is the capital of Saarland?',
+        answers: [
+          'Saarbrücken',
+          'Neunkirchen',
+          'Homburg',
+          'Völkingen',
+          'Zehnschulen',
+        ],
+        image:
+          'https://tourismus.saarbruecken.de/cache/media/attachments/2020/05/77374_x1200_y599_3bfc85_05.jpg',
+        imageAlt: 'View of city hall and surrounds',
+      },
+      {
+        id: 't1q13',
+        question: 'What is the capital of Saxony (Sachsen)?',
+        answers: [
+          'Dresden',
+          'Chemnitz',
+          'Zwickau',
+          'Görlitz',
+          'Saxony is a city-state co-governed by Germany and Guernsey.',
+        ],
+        image:
+          'https://www.dresden.de/media/bilder/testverzeichnis/df_hauptkatalog_0111132.jpg.scaled/f34a3b503b1f45b007296b512a142b91.jpg',
+        imageAlt:
+          'View of Roman Baths with the buildings in the background and a fountain and pond in the foreground',
+      },
+      {
+        id: 't1q14',
+        question: 'What is the capital of Saxony-Anhalt (Sachsen-Anhalt)?',
+        answers: [
+          'Magdeburg',
+          'Halle (Saale)',
+          'Dressau-Roßlau',
+          'Wittenburg',
+          'Bahn-Wahn',
+        ],
+        image:
+          'https://www.magdeburg-tourist.de/media/custom/557_7523_1_g.JPG?1698947569',
+        imageAlt:
+          'Family eating and drinking at a picnic table on the Elbe River',
+      },
+      {
+        id: 't1q15',
+        question: 'What is the capital of Schleswig-Holstein?',
+        answers: [
+          'Kiel',
+          'Lübeck',
+          'Flensburg',
+          'Neumünster',
+          'Meerschweinchen-Holzbeinchen',
+        ],
+        image: 'https://www.kiel.de/images/Schlossgarten_2_9066.jpg',
+        imageAlt: 'Castle gardens with plants and flowers in bloom',
+      },
+      {
+        id: 't1q16',
+        question: 'What is the capital of Thuringia (Thüringen)?',
+        answers: [
+          'Erfurt',
+          'Gera',
+          'Ilmenau',
+          'Jena',
+          'Kickelhahn-Holzbeinchen am Humbach',
+        ],
+        image:
+          'https://www.erfurt-tourismus.de/fileadmin/_processed_/7/8/csm_18_Alte_Synagoge_Westfassade_2019__c_Stadtverwaltung_Erfurt_CC-BY-NC-SA_4173e53cb4.jpg',
+        imageAlt: 'West façade of theThe Old Synagogue, a medieval synagogue',
+      },
+    ],
+  },
+  {
+    id: 't2',
+    topic: 'food and drink',
+    questions: [{
+        id: 't2q1',
+        question: 'What is Spätzle?',
+        answers: [
+          'An egg noodle or dumpling known for its soft but lumpy texture that is a staple of Swabish (Swabia) cuisine',
+          'A cocktail of cola and sparkling apple juice',
+          'A berry found around south eastern German',
+          'What Bavarians shout when dinner is ready',
+          'The small sprinkles on a cake or ice cream',
+        ]
+      },]
+  }
+];
